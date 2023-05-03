d6ea80a7f58e6292895a22982214cc8877deb97d
spartanrex10 <46965967+spartanrex10@users.noreply.github.com>
2023-05-03 02:56:09 -0500

    Movement Animations Fixed (Mostly)
    Movement animations work now, with the only issue being that the side idle animations won't play.

--- Spirit Forge/Assets/PlayerController.cs
+++ Spirit Forge/Assets/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public ContactFilter2D movementFilter;
 
     Vector2 movementInput;
+    SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
     Animator animator;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
@@ -19,6 +20,7 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -32,27 +34,54 @@ public class PlayerController : MonoBehaviour
             bool success = TryMove(movementInput);
 
             if(!success) {
-                success = TryMove(new Vector2(movementInput.x, 0));
-                
-                if(!success) {
-                    success = TryMove(new Vector2(0, movementInput.y));
-                }
+                success = TryMove(new Vector2(movementInput.x, 0));   
+            }
+
+            if(!success) {
+                success = TryMove(new Vector2(0, movementInput.y));
             }
 
             animator.SetBool("isMoving", success);
+            //Check if mocement is horizontal
+            if(movementInput.x != 0 && movementInput.y == 0) {
+                animator.SetBool("horizontal", success);
+                //Set direction of sprite to movement direction
+                if(movementInput.x < 0) {
+                    spriteRenderer.flipX = true;
+                } else if (movementInput.x > 0) {
+                    spriteRenderer.flipX = false;
+                }
+            } else {
+                animator.SetBool("horizontal", false);
+            }
+
+            //Check if movement is up or down
+            if(movementInput.y < 0) {
+                animator.SetBool("down", success);
+            } else {
+                animator.SetBool("down", false);
+            }
+
         } else {
             animator.SetBool("isMoving", false);
         }
 
+
+        
     }
 
     private bool TryMove(Vector2 direction) {
-        int count = rb.Cast(direction, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset);
-            
-        if(count == 0) {
-            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
-            return true;
+        if(direction != Vector2.zero) {
+            int count = rb.Cast(direction, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset);
+                
+            if(count == 0) {
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            } else {
+                return false;
+            }
         } else {
+            //Can't move if there's no direction to move in
             return false;
         }
     }
